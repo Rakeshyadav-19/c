5f00c908a62c154e44eefa8eca17b2316420bed6
rakesyadav19 <rakeshyadavry087@gmail.com>
2024-09-10 08:44:53 +0530

    code change

--- DAA/8-queen-backTrack.cpp
+++ DAA/8-queen-backTrack.cpp
@@ -1,50 +1,63 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
 const int N = 8;
 
-bool isSafe(vector<vector<int> >& board, int row, int col)
-{
-	for (int x = 0; x < col; x++)
-		if (board[row][x] == 1)
-			return false;
-	for (int x = row, y = col; x >= 0 && y >= 0; x--, y--)
-		if (board[x][y] == 1)
-			return false;
-	for (int x = row, y = col; x < N && y >= 0; x++, y--)
-		if (board[x][y] == 1)
-			return false;
-	return true;
+// Check if it's safe to place a queen at board[row][col]
+bool isSafe(int board[N][N], int row, int col) {
+    // Check this row on the left side
+    for (int x = 0; x < col; x++)
+        if (board[row][x] == 1)
+            return false;
+
+    // Check upper diagonal on the left side
+    for (int x = row, y = col; x >= 0 && y >= 0; x--, y--)
+        if (board[x][y] == 1)
+            return false;
+
+    // Check lower diagonal on the left side
+    for (int x = row, y = col; x < N && y >= 0; x++, y--)
+        if (board[x][y] == 1)
+            return false;
+
+    return true;
 }
 
-bool solveNQueens(vector<vector<int> >& board, int col)
-{
-	if (col == N) {
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++)
-				cout << board[i][j] << " ";
-			cout << endl;
-		}
-		cout << endl;
-		return true;
-	}
-	for (int i = 0; i < N; i++) {
-		if (isSafe(board, i, col)) {
-			board[i][col] = 1;
-			if (solveNQueens(board, col + 1))
-				return true;
-			board[i][col] = 0;
-		}
-	}
-	return false;
+// Solve the N-Queen problem
+bool solveNQueens(int board[N][N], int col) {
+    if (col == N) {
+        // Print the solution
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++)
+                cout << board[i][j] << " ";
+            cout << endl;
+        }
+        cout << endl;
+        return true;
+    }
+
+    // Consider this column and try placing this queen in all rows one by one
+    for (int i = 0; i < N; i++) {
+        if (isSafe(board, i, col)) {
+            // Place queen at board[i][col]
+            board[i][col] = 1;
+
+            // Recur to place rest of the queens
+            if (solveNQueens(board, col + 1))
+                return true;
+
+            // If placing queen at board[i][col] doesn't lead to a solution, backtrack
+            board[i][col] = 0;
+        }
+    }
+    return false;
 }
 
-int main()
-{
-	vector<vector<int> > board(N, vector<int>(N, 0));
-	if (!solveNQueens(board, 0))
-		cout << "No solution found";
-	return 0;
+int main() {
+    int board[N][N] = {0};  // Initialize board with 0s
+
+    if (!solveNQueens(board, 0))
+        cout << "No solution found";
+
+    return 0;
 }
