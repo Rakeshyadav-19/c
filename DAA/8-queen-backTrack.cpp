d860eb7efd8fcd2dc8048d5b707386524d846f4a
rakesyadav19 <rakeshyadavry087@gmail.com>
2024-09-10 13:15:54 +0530

    DAA done

--- DAA/8-queen-backTrack.cpp
+++ DAA/8-queen-backTrack.cpp
@@ -4,7 +4,8 @@ using namespace std;
 const int N = 8;
 
 // Check if it's safe to place a queen at board[row][col]
-bool isSafe(int board[N][N], int row, int col) {
+bool isSafe(int board[N][N], int row, int col)
+{
     // Check this row on the left side
     for (int x = 0; x < col; x++)
         if (board[row][x] == 1)
@@ -24,10 +25,13 @@ bool isSafe(int board[N][N], int row, int col) {
 }
 
 // Solve the N-Queen problem
-bool solveNQueens(int board[N][N], int col) {
-    if (col == N) {
+bool solveNQueens(int board[N][N], int col)
+{
+    if (col == N)
+    {
         // Print the solution
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++)
+        {
             for (int j = 0; j < N; j++)
                 cout << board[i][j] << " ";
             cout << endl;
@@ -37,8 +41,10 @@ bool solveNQueens(int board[N][N], int col) {
     }
 
     // Consider this column and try placing this queen in all rows one by one
-    for (int i = 0; i < N; i++) {
-        if (isSafe(board, i, col)) {
+    for (int i = 0; i < N; i++)
+    {
+        if (isSafe(board, i, col))
+        {
             // Place queen at board[i][col]
             board[i][col] = 1;
 
@@ -46,15 +52,16 @@ bool solveNQueens(int board[N][N], int col) {
             if (solveNQueens(board, col + 1))
                 return true;
 
-            // If placing queen at board[i][col] doesn't lead to a solution, backtrack
+            // If placiwhwhng queen at board[i][col] doesn't lead to a solution, backtrack
             board[i][col] = 0;
         }
     }
     return false;
 }
 
-int main() {
-    int board[N][N] = {0};  // Initialize board with 0s
+int main()
+{
+    int board[N][N] = {0}; // Initialize board with 0s
 
     if (!solveNQueens(board, 0))
         cout << "No solution found";
